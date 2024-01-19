0f93a538dbd379b3ff60a5ea347802d4a7d0c54f
Shruti <shrutigawande1910@gmail.com>
2024-01-19 12:23:36 +0530

    Added Backend data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,33 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "Wig",
+    productImg:"https://cdn.shopify.com/s/files/1/0688/3370/6274/products/89C86233-1F06-4BC3-96C2-DEE6C746397D-768x1152_960x_crop_center.webp?v=1673609089"
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "Ponytail Extensions",
+    productImg:"https://cdn.shopify.com/s/files/1/0258/2531/2848/products/Ponytail-extensions_2ebcfb1e-f147-4858-9a85-682d38e4b343.jpg?v=1680006343&width=1100"
+   
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "Single Clip Highlights",
+    productImg:"https://cdn.shopify.com/s/files/1/0591/6422/9806/products/LRFeaturedImage_28040c6f-df53-41a1-bd70-78ce2a7e6e56_800x.jpg?v=1660804194"
+    
   },
+  
+  {
+    _id: uuid(),
+    categoryName: "Curly Hair",
+    productImg:"https://cdn.shopify.com/s/files/1/0688/3370/6274/products/Curlytopknot_960x_crop_center.jpg?v=1674476325"
+   
+  },
+  {
+    _id: uuid(),
+    categoryName: "Clip-In Bangs",
+    productImg:"https://cdn.shopify.com/s/files/1/0591/6422/9806/files/COVERIMAGE_9a7fbca3-2d52-46b4-86eb-9956ec98680a_800x.jpg?v=1684320444"
+    
+  },
+  
 ];
--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Shruti",
+    lastName: "Gawande",
+    email: "shrutigawande@gmail.com",
+    password: "shruti12345",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
